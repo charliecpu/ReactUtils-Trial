c8b0f5757a729a499be58df34d881e5cf671c021
mordechaigellis <mgellis@cpucodeschool.com>
2024-05-29 18:22:00 +0000

    ran npx in terminal

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'), // Ensure this path is correct
+      name: 'cpureactutils',
+      formats: ['es', 'umd'], // Ensure UMD format is included
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      // Ensure to externalize deps that shouldn't be bundled
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  },
+  plugins: [react()]
+});
